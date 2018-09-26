f41721cfe3b7d4ccb103356a268fd5f5ed4461c0
Joyesh <joyesh708@gmail.com>
2018-09-26 00:59:21 +0000

    add settings menu

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ import os
 import Adafruit_CharLCD as LCD
 
 # variables
+settings=["Backlight", "Repeat"]
 run = True
+PreviousPlayingState = False
+PreviousBacklightState = True
 repeat = False
+backlight = True
 state = 0
 # states:
 # 0 - File Explorer
@@ -48,18 +52,19 @@ lcd.clear()
 # scrolling text for long filenames
 def renderscrolltext(text, cx, cy):
     if len(text) < 32:
-        lcd.set_cursor(cx, cy)
+        #lcd.set_cursor(cx, cy)
         lcd.message(text[0:15])
 
 # LCD UPDATE
 def lcd_update():
+    lcd.clear()
     if state == 0:
         rnge = []
         # prevent index out of range errors
         if selected + 2 <= len(dirlist):
             rnge = range(selected, selected + 2)
         elif selected <= len(dirlist):
-            rnge = range(selected, selected+1)
+            rnge = range(selected, selected + 1)
         
         # render
         index = 0
@@ -71,7 +76,6 @@ def lcd_update():
             index + 1
 
     if state == 1:
-        lcd.clear()
         if player.is_playing():
             lcd.message("\x01 Now Playing\n" + filename)
         else:
@@ -80,6 +84,28 @@ def lcd_update():
             lcd.set_cursor(14,0)
             lcd.message("\x03\x04")
 
+    if state == 2:
+        rnge = []
+        # prevent index out of range errors
+        if selected + 2 <= len(settings):
+            rnge = range(selected, selected + 2)
+        elif selected <= len(settings):
+            rnge = range(selected, selected + 1)
+        # render
+        for x in rnge:
+            lcd.message(settings[x])
+            if settings[x] == "Backlight":
+                if backlight:
+                    lcd.message(" on\n")
+                else:
+                    lcd.message(" off\n")
+            
+            elif settings[x] == "Repeat":
+                if repeat:
+                    lcd.message(" on\n")
+                else:
+                    lcd.message(" off\n")
+
 
 dirlist = os.listdir(path)
 dirlist.insert(0, "..")
@@ -87,8 +113,17 @@ lcd_update()
 
 # MAIN UPDATE
 while run:
+    if not PreviousPlayingState == player.is_playing():
+        lcd_update()
+    if not PreviousBacklightState == backlight:
+        if backlight:
+            lcd.set_color(1,1,1)
+        else:
+            lcd.set_color(0,0,0)
+
+    PreviousBacklightState = backlight
+    PreviousPlayingState = player.is_playing()
     if lcd.is_pressed(LCD.SELECT):
-        lcd.clear()
         # SELECT/PLAY/PAUSE
         if state == 0:
             # if path is directory
@@ -114,9 +149,6 @@ while run:
                 player.play()
                 # set state to player info
                 state = 1
-            
-            # update lcd
-            lcd_update()
         
         elif state == 1:
             if player.is_playing():
@@ -124,7 +156,13 @@ while run:
             else:
                 player.play()
 
-            lcd_update()
+        elif state == 2:
+            if settings[selected] == "Backlight":
+                backlight = not backlight
+            if settings[selected] == "Repeat":
+                repeat = not repeat
+
+        lcd_update()
     
     elif lcd.is_pressed(LCD.LEFT):
         # STOP
@@ -142,20 +180,29 @@ while run:
         # if selected - 1 is not negative (prevent out of range errors)
         if selected - 1 >= 0:
             selected = selected - 1
-            lcd.clear()
             lcd_update()
 
     elif lcd.is_pressed(LCD.DOWN):
         # DOWN IN MENU
         # if selected + 1 is not out of list range
-        if selected + 1 <= len(os.listdir(path)):
-            selected = selected + 1
-            lcd.clear()
-            lcd_update()
+        if state == 0:
+            if selected + 1 <= len(os.listdir(path)):
+                selected = selected + 1
+
+        elif state == 2:
+            if selected + 2 <= len(settings):
+                selected = selected + 1
+
+        lcd_update()
 
     elif lcd.is_pressed(LCD.RIGHT):
         # SETTINGS MENU
-        state = 2
+        if state == 2:
+            selected = 0
+            state = 0
+        else:
+            selected = 0
+            state = 2
         lcd_update()
 
     time.sleep(0.1)
